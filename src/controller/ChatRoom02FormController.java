d86d7ffc0a3787ebe141771ab5d87f251c4f3d0c
JeeWaa <jeewa228@gmail.com>
2022-08-16 16:46:30 +0530

    client

--- src/controller/ChatRoom02FormController.java
+++ src/controller/ChatRoom02FormController.java
@@ -4,10 +4,42 @@ import com.jfoenix.controls.JFXTextArea;
 import com.jfoenix.controls.JFXTextField;
 import javafx.scene.input.MouseEvent;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class ChatRoom02FormController {
     public JFXTextArea txtChat;
     public JFXTextField txtMessage;
+    Socket socket = null;
 
-    public void send(MouseEvent mouseEvent) {
+    public void initialize() {
+        new Thread(()->{
+            try {
+                socket = new Socket("localhost",8000);
+                System.out.println("Client started");
+
+                while (true) {
+                    InputStreamReader inputStreamReader = new InputStreamReader(socket.getInputStream());
+                    BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                    String read = bufferedReader.readLine();
+                    System.out.println(read);
+                    txtChat.appendText(read + "\n");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
+    public void send(MouseEvent mouseEvent) throws IOException {
+        String message = txtMessage.getText();
+
+        txtChat.appendText("Me : " + message + "\n");
+        PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
+        printWriter.println("Kamal : " + message + "\n");
+        printWriter.flush();
     }
 }
--- src/controller/ServerFormController.java
+++ src/controller/ServerFormController.java
@@ -12,29 +12,32 @@ import java.net.Socket;
 
 public class ServerFormController {
     public JFXTextArea txtData;
-    Socket socket = null;
+    Socket accept = null;
 
     public void initialize() {
         new Thread(()->{
             try {
-                ServerSocket serverSocket = new ServerSocket(2000);
-                System.out.println("Server started");
-                socket = serverSocket.accept();
-                System.out.println("Client connected");
+                ServerSocket serverSocket = new ServerSocket(8000);
+                txtData.appendText("Server started\n");
+                accept = serverSocket.accept();
+                txtData.appendText("Client connected\n");
+                while (true) {
+                    InputStreamReader inputStreamReader = new InputStreamReader(accept.getInputStream());
+                    BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                    String read = bufferedReader.readLine();
+                    txtData.appendText(read + "\n");
 
-                InputStreamReader inputStreamReader = new InputStreamReader(socket.getInputStream());
-                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-                String read = bufferedReader.readLine();
-                System.out.println(read);
+                    PrintWriter printWriter = new PrintWriter(accept.getOutputStream());
+                    printWriter.println("success\n");
+                    printWriter.flush();
+                }
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        });
+        }).start();
     }
 
     public void close(MouseEvent mouseEvent) throws IOException {
-        PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
-        printWriter.println(txtData.getText());
-        printWriter.flush();
+
     }
 }
